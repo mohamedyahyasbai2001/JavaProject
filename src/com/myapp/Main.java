814ee1c42d0dabb0aaf43e3b7de0dc6bf3b93474
Mohamed Yahya <mohamedyahyasbai@gmail.com>
2024-06-02 17:25:05 +0200

    Initial commit.

---
+++ src/com/myapp/Main.java
@@ -0,0 +1,10 @@
+package com.myapp;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int number : numbers) {
+            System.out.println("Number: " + number);
+        }
+    }
+}
